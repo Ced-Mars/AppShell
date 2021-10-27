537ad219047830da2187bcaa895b304425153622
SOULARD Cédric <cedric.soulard.external@airbus.com>
2021-10-27 14:47:37 +0200

    Server called on PORT 4000

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require('path');
 
 const app = express(); // create express app
-const PORT = 5005;
+const PORT = process.env.PORT || 4000;
 
 app.use(express.static("dist"));
 
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
-// start express server on port 5000
+// start express server on port 4000
 app.listen(PORT, () => {
   console.log("server started on port ", PORT);
 });
